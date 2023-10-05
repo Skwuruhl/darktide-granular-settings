5f3f2de3f19056056bd41db74e0340b0deef8638
Skwuruhl <smmarion11@gmail.com>
2023-10-04 20:20:56 -0700

    updated for Patch #13
    patch #13 added two new sensitivity sliders and increased sensitivity caps to 10. I lowered them back to 2.

--- granular_settings/scripts/mods/granular_settings/granular_settings.lua
+++ granular_settings/scripts/mods/granular_settings/granular_settings.lua
@@ -3,8 +3,9 @@ local mod = get_mod("granular_settings")
 local OptionsUtilities = require("scripts/utilities/ui/options")
 
 mod:hook(OptionsUtilities, "create_value_slider_template", function(func, params)
-    if params.id == "mouse_look_scale" then
+    if params.id == "mouse_look_scale" or params.id == "mouse_look_scale_ranged" or params.id == "mouse_look_scale_ranged_alternate_fire" then
         params.min_value = 0.01
+        params.max_value = 2
         params.num_decimals = 2
         params.step_size_value = 0.01
     end
@@ -16,8 +17,8 @@ mod:hook_require("scripts/settings/options/render_settings", function(instance)
         if v.id == "gameplay_fov" then
             v.num_decimals = 1
             v.step_size_value = 0.1
-            v.min_value = v.min_value + 20
-            v.max_value = v.max_value + 20
+            v.min_value = 65
+            v.max_value = 105
         end
     end
 end)
